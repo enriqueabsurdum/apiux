cafea5ddfb24e756e85907e46a5b49640911df64
Enrique <enriqueabsurdum@gmail.com>
2020-07-16 12:54:07 -0400

    Creation: Author and Book model class

--- apps/api/admin.py
+++ apps/api/admin.py
@@ -1,1 +1,10 @@
 """API admin."""
+
+# Django
+from django.contrib import admin
+
+# Models
+from .models import Author, Book
+
+admin.site.register(Author)
+admin.site.register(Book)
--- apps/api/models.py
+++ apps/api/models.py
@@ -1,1 +1,53 @@
 """API models."""
+
+# Django
+from django.db import models
+
+# Utils
+from apps.utils.models import BaseModel
+
+
+class Author(BaseModel):
+    """Model class Author."""
+
+    name = models.CharField(
+        unique=True,
+        verbose_name='nombre',
+        max_length=128
+    )
+    nationality = models.CharField(
+        verbose_name='nacionalidad',
+        max_length=64
+    )
+
+    class Meta:
+        verbose_name = 'autor'
+        verbose_name_plural = 'autores'
+
+    def __str__(self):
+        """Returns the name of the created Author."""
+        return self.name
+
+
+class Book(BaseModel):
+    """Model class Book."""
+
+    title = models.CharField(
+        unique=True,
+        verbose_name='título',
+        max_length=256
+    )
+    author = models.ForeignKey(
+        'Author',
+        verbose_name='autor',
+        related_name='books',
+        on_delete=models.CASCADE
+    )
+
+    class Meta:
+        verbose_name = 'libro'
+        verbose_name_plural = 'libros'
+
+    def __str__(self):
+        """Returns the title of the created Book."""
+        return self.title
